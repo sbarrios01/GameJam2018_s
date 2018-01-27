28b52b2123e70dab29ae00015935b04f15290e16
carlos carvajal vazquez <carlos.caruajal@karaokulta.com>
2018-01-27 01:39:40 -0600

    Corregido error de movimiento invertido

--- GameJam2018/Assets/Scripts/Player.cs
+++ GameJam2018/Assets/Scripts/Player.cs
@@ -7,21 +7,23 @@ public class Player : MonoBehaviour
     private float _EjeX;      //movimiento en el eje x por velocidad
     private float _EjeY;      //movimiento en el eje y por velocidad
 
-    private float LocationX = 1.0f;
-    private float LocationY = 1.0f;
+    public float LocationX;
+    public float LocationY;
 
     [SerializeField]
     private float _Speed;      //velocidad en cuandros por segundo
                                // Use this for initialization
     void Start()
     {
-        LocationX = transform.position.x;
-        LocationY = transform.position.y;
+        _Speed = 6.0f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        LocationX = transform.position.x;
+        LocationY = transform.position.y;
         Movement();
     }
 
@@ -31,22 +33,5 @@ public class Player : MonoBehaviour
         _EjeY = Input.GetAxis("Vertical");
 
         transform.Translate(new Vector3(_EjeX, _EjeY, 0f) * _Speed * Time.deltaTime);
-
-        if (transform.position.x > 8.30)
-        {
-            transform.position = new Vector3(8.30f, transform.position.y, 0f);
-        }
-        else if (transform.position.x < -8.30)
-        {
-            transform.position = new Vector3(-8.30f, transform.position.y, 0f);
-        }
-        if (transform.position.y > 0)
-        {
-            transform.position = new Vector3(transform.position.x, 0f, 0f);
-        }
-        else if (transform.position.y < -4.20)
-        {
-            transform.position = new Vector3(transform.position.x, -4.20f, 0f);
-        }
     }
 }
