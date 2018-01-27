4f58569bf49173db0b7ddc5f97455a1d8b72c306
urielrobles <urielrobles46@gmail.com>
2018-01-27 04:34:07 -0600

    Add files via upload
    Lo que llevamos

---
+++ Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 0.05f;
+    private Vector3 posInit;
+
+
+
+    void Start()
+    {
+        speed = Mathf.Abs(speed);
+        posInit = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        //Estamos evitando que se pulse las teclas hacia arriba y hacia abajo
+        if (Mathf.Abs(h) > 0 && Mathf.Abs(v) > 0)
+            return;
+        //-------------------------------------------------------------------
+        if (Mathf.Abs(h) > 0)
+            Movement(h, 0f);
+        else if (Mathf.Abs(v) > 0)
+            Movement(0f, v);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+            transform.position = posInit;
+    }
+
+    void Movement(float h, float v)
+    {
+        Vector3 pos = transform.position;
+        pos.x += (h * speed);
+        pos.y += (v * speed);
+        transform.position = pos;
+    }
+
+}
---
+++ Scripts/PlayerControl.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public float moveSpeed;
+    private Rigidbody2D m_rigidBody2d;
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+    public Animator m_animator;
+    public AudioClip soundMuerte;
+    public AudioClip soundCoin;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_rigidBody2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        moveInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        moveVelocity = moveInput * moveSpeed;
+
+        /*Vector3 finalPos = transform.position;
+
+        finalPos.x = Mathf.Min(7f, Mathf.Max(-7f, finalPos.x));
+        finalPos.y = Mathf.Min(3.5f, Mathf.Max(-3.5f, finalPos.y));
+
+        transform.position = finalPos;*/
+
+    }
+
+    void FixedUpdate()
+    {
+        //if (!perdiste)
+        //{
+            m_rigidBody2d.velocity = moveVelocity;
+        //}
+        //else
+        //{
+        //    m_rigidBody2d.velocity = Vector2.zero;
+        //}
+    }
+
+    /*void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy" && !m_generalVars.perdiste)
+        {
+            m_generalVars.perdiste = true;
+            m_animator.SetTrigger("perdiste");
+            m_generalVars.DetenerlMusica();
+            m_generalVars.ReproducirSFX(soundMuerte);
+        }
+        else if (other.gameObject.tag == "HopeAzul")
+        {
+            m_generalVars.puntos += 10;
+            Destroy(other.gameObject);
+            m_generalVars.ReproducirSFX(soundCoin);
+        }
+    }*/
+
+}
