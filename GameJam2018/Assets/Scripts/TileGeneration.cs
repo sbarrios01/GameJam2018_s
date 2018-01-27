b4cd91218a2942a6320957ff9ce5e929c24ba81a
Chaika-PC\Marco <marcokiller@live.com.mx>
2018-01-27 03:08:17 -0600

    Solución a errores de compilación del tilegeneration.

--- GameJam2018/Assets/Scripts/TileGeneration.cs
+++ GameJam2018/Assets/Scripts/TileGeneration.cs
@@ -7,13 +7,13 @@ public class TileGeneration : MonoBehaviour {
     //Actualmente los tiles del juego son de 30*30 pixeles y el espacio del juego es de 600*600 pixeles
     //Por lo cual nos caben 20 tiles horizontal y verticalmente de ser modificado hay que tener el siguiente codigo en mente
     private int maxTiles = 20;
-    private int tileSize = 30;
+    private float tileSize = 1;
 
 
     public GameObject wall;
     public GameObject empty;
     //                           1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0
-    private int level[20][20] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //1
+    private int[] map= new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //1
                                 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //2
                                 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //3
                                 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //4
@@ -32,13 +32,20 @@ public class TileGeneration : MonoBehaviour {
                                 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //7
                                 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //8
                                 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //9
-                                ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}//0
+                                ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};//0
     void Awake()
     {
+                                          //x     y   z
+        Vector3 coordenadas = new Vector3(-15, -15, 0);
         for (int i = 0; i < maxTiles; i++)
         {
-            for()
-            Instantiate<GameObject>(wall,, Quaternion.identity);
+            for(int j = 0; j < maxTiles; j++)
+            {
+                Instantiate<GameObject>(wall,coordenadas,Quaternion.identity);
+                coordenadas.y += tileSize;
+            }
+            coordenadas.x += tileSize;
+            coordenadas.y = -15;
         } 
     }
 	// Use this for initialization
