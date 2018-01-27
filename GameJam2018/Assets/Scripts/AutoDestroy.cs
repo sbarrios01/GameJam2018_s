d8b29e367a8339649c0326d2ba73221fe0e5c7e8
carlos carvajal vazquez <carlos.caruajal@karaokulta.com>
2018-01-27 03:12:11 -0600

    Stages changes

---
+++ GameJam2018/Assets/Scripts/AutoDestroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour {
+
+
+    public float Timer;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(this.gameObject, Timer);
+	}
+	
+	// Update is called once per frame
+
+}
