766d7f25e153f7b8d6c2a12ea121d1f1edfc378d
Chaika-PC\Marco <marcokiller@live.com.mx>
2018-01-27 05:34:16 -0600

    Cambios a tile generation.

--- GameJam2018/Assets/Scripts/TileGeneration.cs
+++ GameJam2018/Assets/Scripts/TileGeneration.cs
@@ -7,23 +7,23 @@ public class TileGeneration : MonoBehaviour {
     //Actualmente los tiles del juego son de 30*30 pixeles y el espacio del juego es de 600*600 pixeles
     //Por lo cual nos caben 20 tiles horizontal y verticalmente de ser modificado hay que tener el siguiente codigo en mente
     private int maxTiles = 20;
-    private float tileSize = 1;
+    private float tileSize = 120;
 
 
     public GameObject wall;
     public GameObject empty;
     //                           1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0
-    private int[] map= new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //1
-                                ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //2
-                                ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //3
-                                ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //4
-                                ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //5
+    private int[] map= new int[]{1,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,0,0,0 //1
+                                ,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0 //2
+                                ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0 //3
+                                ,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0 //4
+                                ,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0 //5
                                 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //6
                                 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //7
                                 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //8
                                 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //9
-                                ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //0
-                                ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //1
+                                ,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0 //0
+                                ,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0 //1
                                 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //2
                                 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //3
                                 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 //4
@@ -35,17 +35,27 @@ public class TileGeneration : MonoBehaviour {
                                 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};//0
     void Awake()
     {
-                                          //x     y   z
-        Vector3 coordenadas = new Vector3(-15, -15, 0);
+        int k = 0;
+        //x     y   z
+        Vector3 coordenadas = new Vector3(-1150,1150, 0);
         for (int i = 0; i < maxTiles; i++)
         {
             for(int j = 0; j < maxTiles; j++)
             {
-                Instantiate<GameObject>(wall,coordenadas,Quaternion.identity);
-                coordenadas.y += tileSize;
+                if (map[k] == 0)
+                {
+
+                    Instantiate<GameObject>(wall, coordenadas, Quaternion.identity);
+                }
+                else if(map[k] == 1)
+                {
+                    Instantiate<GameObject>(empty, coordenadas, Quaternion.identity);
+                }
+                coordenadas.x += tileSize;
+                k++;
             }
-            coordenadas.x += tileSize;
-            coordenadas.y = -15;
+            coordenadas.y -= tileSize;
+            coordenadas.x = -1150;
         } 
     }
 	// Use this for initialization
