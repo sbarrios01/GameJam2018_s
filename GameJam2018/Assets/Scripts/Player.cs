52ab9bb2a4c70fe6ed9e4d1d675651dfe7334393
Sergio Barrios <sergio.barrios@karaokulta.com>
2018-01-27 05:10:53 -0600

    Corrigiendo el movimiento del jugador.

--- GameJam2018/Assets/Scripts/Player.cs
+++ GameJam2018/Assets/Scripts/Player.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
+    
     private float _EjeX;      //movimiento en el eje x por velocidad
-    [SerializeField]
     private float _EjeY;      //movimiento en el eje y por velocidad
 
-    public float LocationX;
-    public float LocationY;
+    private float LocationX;
+	private float LocationY;
     private Vector3 _LastMovement;
     [SerializeField]
     private GameObject _Atack;
 
     [SerializeField]
-    private float _Speed;      //velocidad en cuandros por segundo
+    private int _Speed;      //velocidad en cuandros por segundo
                                // Use this for initialization
+	[SerializeField]
+	private int _distance_player_attack;
+
+	const int Screen_Width = 600;
+	const int Screen_Heigth = 800;
     void Start()
     {
-        _Speed = 6.0f;
+        
 
     }
 
@@ -47,38 +51,38 @@ public class Player : MonoBehaviour
         _EjeY = Input.GetAxis("Vertical");
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            _LastMovement = new Vector3(-1, 0, 0);
+            _LastMovement = new Vector3(-_distance_player_attack, 0, 0);
         }
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            _LastMovement = new Vector3(1, 0, 0);
+			_LastMovement = new Vector3(_distance_player_attack, 0, 0);
         }
         if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.UpArrow))
         {
-            _LastMovement = new Vector3(0, -1, 0);
+			_LastMovement = new Vector3(0, -_distance_player_attack, 0);
         }
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.DownArrow))
         {
-            _LastMovement = new Vector3(0, 1, 0);
+			_LastMovement = new Vector3(0, _distance_player_attack, 0);
         }
 
-        transform.Translate(new Vector3(_EjeX, _EjeY, 0f) * _Speed * Time.deltaTime);
+        transform.Translate(new Vector3(_EjeX, _EjeY, 0) * _Speed * Time.deltaTime);
 
-        if (transform.position.x > 6.15)
+		if (transform.position.x > Screen_Width)
         {
-            transform.position = new Vector3(6.15f, transform.position.y, 0f);
+			transform.position = new Vector3(Screen_Width, transform.position.y, 0);
         }
-        else if (transform.position.x < -6.15)
+		else if (transform.position.x < -Screen_Width)
         {
-            transform.position = new Vector3(-6.15f, transform.position.y, 0f);
+			transform.position = new Vector3(-Screen_Width, transform.position.y, 0);
         }
-        if (transform.position.y > 4.5)
+		if (transform.position.y > Screen_Heigth)
         {
-            transform.position = new Vector3(transform.position.x, 4.5f, 0f);
+			transform.position = new Vector3(transform.position.x, Screen_Heigth, 0);
         }
-        else if (transform.position.y < -4.5)
+		else if (transform.position.y < -Screen_Heigth)
         {
-            transform.position = new Vector3(transform.position.x, -4.5f, 0f);
+			transform.position = new Vector3(transform.position.x, -Screen_Heigth, 0);
         }
 
     }
