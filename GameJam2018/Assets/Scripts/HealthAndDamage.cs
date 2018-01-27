d78c988af6eccfb47404ba88e2d69e3e691b46da
Chaika-PC\Marco <marcokiller@live.com.mx>
2018-01-27 17:46:16 -0600

    En este punto se hizo la conexion con unity collab

---
+++ GameJam2018/Assets/Scripts/HealthAndDamage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthAndDamage : MonoBehaviour {
+
+    public int playerHitPoints;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
